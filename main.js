523fb9d2ad92fdb5fbee37bdc0b3f17c3301254b
Wibren Van Assche <r0368680@student.thomasmore.be>
2016-12-11 14:03:29 +0100

    denk klaar maar krijg error

--- main.js
+++ main.js
@@ -8,18 +8,18 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 var BASE_URL = "https://web-ims.thomasmore.be/datadistribution/API/2.0";
 var Settings = function (url) {
-	this.url = BASE_URL + url;
-	this.method = "GET";
-	this.qs = {format: 'json'};
-	this.headers = {
-		authorization: "Basic aW1zOno1MTJtVDRKeVgwUExXZw=="
-	};
+    this.url = BASE_URL + url;
+    this.method = "GET";
+    this.qs = {format: 'json'};
+    this.headers = {
+        authorization: "Basic aW1zOno1MTJtVDRKeVgwUExXZw=="
+    };
 };
 
 var Drone = function (id, name, mac) {
-	this._id = id
-	this.name = name;
-	this.mac = mac;
+    this._id = id
+    this.name = name;
+    this.mac = mac;
 };
 
 var dronesSettings = new Settings("/drones?format=json");
@@ -27,26 +27,33 @@ var dronesSettings = new Settings("/drones?format=json");
 dal.clearDrone();
 
 request(dronesSettings, function (error, response, dronesString) {
-	var drones = JSON.parse(dronesString);
-	console.log(drones);
-	console.log("***************************************************************************");
-	drones.forEach(function (drone) {
-		var droneSettings = new Settings("/drones/" + drone.id + "?format=json");
-		request(droneSettings, function (error, response, droneString) {
-			var drone = JSON.parse(droneString);
-			//dal.insertDrone(new Drone(drone.id, drone.name, drone.mac_address));
-                       
-                        var dronespecssettings = new Settings ("/files?drone_id.is=" + drone.id + "&format=json")
-                    request(dronespecssettings, function (error, response, dronespecsString) {
-                        
-			var dronespec = JSON.parse(dronespecsString);
-			//dal.insertDrone(new Drone(dronespecsString.id));
-                        console.log(dronespec[0].id);
-		});
-		});
-                
-                    
-	});
+    var drones = JSON.parse(dronesString);
+    //console.log(drones);
+    console.log("***************************************************************************");
+    drones.forEach(function (drone) {
+        var droneSettings = new Settings("/drones/" + drone.id + "?format=json");
+        request(droneSettings, function (error, response, droneString) {
+            var drone = JSON.parse(droneString);
+            //dal.insertDrone(new Drone(drone.id, drone.name, drone.mac_address));
+
+            var dronelocsettings = new Settings("/files?drone_id.is=" + drone.id + "&format=json");
+            request(dronelocsettings, function (error, response, dronelocString) {
+                var droneloc = JSON.parse(dronelocString);
+
+
+                droneloc.forEach(function (filedetails) {
+                    var filedetailsSettings = new Settings('/files' + filedetails.id + "?format=json");
+                    request(filedetailsSettings, function (error, response, filedetailsstring) {
+                        var filedeets = JSON.parse(filedetailsstring);
+                        dal.insertDrone(new Drone(drone.id, drone.name, drone.mac_address, drone.location, filedeets.date_first_record, filedeets.date_last_record));
+                    });
+                });
+
+            });
+        });
+
+
+    });
 });
 
 console.log("Hello World!");
